45bbb78954a8abca9f3dd8a663969022cad68ea5
taimoor-ghori <taimoor@slash.co>
2024-05-08 05:49:33 +0700

    forms done

---
+++ client/src/pages/app/customers/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CustomerForm from './CustomerForm'
+
+function index() {
+  return (
+    <div>
+        <CustomerForm />
+    </div>
+  )
+}
+
+export default index
---
+++ client/src/pages/app/vouchers/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import VouchersForm from './VouchersForm'
+
+function index() {
+  return (
+    <div><VouchersForm /></div>
+  )
+}
+
+export default index
